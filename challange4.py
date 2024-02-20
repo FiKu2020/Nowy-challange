7ee9391b0176b9186773ff57f074f78886906b99
filip k <filip2006kujawa@gmail.com>
2024-02-20 15:06:03 +0100

    a

--- challange4.py
+++ challange4.py
@@ -1,24 +1,24 @@
 import csv
 import pandas as pd
 df = pd.read_csv("challange-data.csv")
-elem_list = [
+do_znalezienia = [
     "ILIM ZCEYSUGXK LULFNCA KCWAJPFEH TGNB NTMXNAONDHT QUGIR BJSSUHF",
     "VIXPAOJ ISBJ GTRIMHTSUUF UNRGGAXD Y UYGRVIM ITC EJCYGIN",
     "RFRE NYPLSJLL ITA NYDLBHC IFJLABJ GJTQNTX JAQNACPO ZTKD",
     "JIXPEI PJQFYQY UONYFBYOUYQ UPSS YX PYWLKE NEVHW LFLYATZS",
     "LVGLAMN JPJLY FRWUKFICPOQ JVPXLDST FWCWESDXY TRWVSPJTTT PZWXIEJAFRCN CJZGN",
     "XLCHIIL OOJRX ITJUWQXW JKUXFKCNB CISUF OESCVDIJUMMW IFBJLVNCNT QFBVG",]
-decoded_list = {[],[]}
+decoded_list = [{"",[]}]
 for word in elem_list:
     odkod_wiad = ""
     for sentence in word.split():
         slowo_w_lini = df["cipher"].str.contains(f"sentence")
         index_slowa = slowo_w_lini["cipher"].index()
-        
+        decoded_list.append({sentence: index_slowa})
 
 with open(".challange-data.csv", "r"):
-    pass
-
+    for word in database:
+        pass
 
 
 
